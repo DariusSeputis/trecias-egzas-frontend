0a057c14e7cb3c4b54e0152c5022de415bf9982d
DariusSeputis <82835894+DariusSeputis@users.noreply.github.com>
2021-09-15 19:11:01 +0300

    bonus slashu pridejau nx

--- scripts/index.js
+++ scripts/index.js
@@ -10,22 +10,22 @@ const getAmountJokesFromCategoryTag = document.querySelector(
 );
 // Funtions
 function fetchTenJokes() {
-  fetch('https://treciasegzassendien.herokuapp.com//api/jokes/')
+  fetch('https://treciasegzassendien.herokuapp.com/api/jokes/')
     .then((res) => res.json())
     .then((data) => console.log(data));
 }
 function fetchAmountJokes() {
-  fetch('https://treciasegzassendien.herokuapp.com//api/jokes/4')
+  fetch('https://treciasegzassendien.herokuapp.com/api/jokes/4')
     .then((res) => res.json())
     .then((data) => console.log(data));
 }
 function fetchTenJokesFromCategory() {
-  fetch('https://treciasegzassendien.herokuapp.com//api/jokes/science')
+  fetch('https://treciasegzassendien.herokuapp.com/api/jokes/science')
     .then((res) => res.json())
     .then((data) => console.log(data));
 }
 function AmountJokesFromCategory() {
-  fetch('https://treciasegzassendien.herokuapp.com//api/jokes/science/3')
+  fetch('https://treciasegzassendien.herokuapp.com/api/jokes/science/3')
     .then((res) => res.json())
     .then((data) => console.log(data));
 }
